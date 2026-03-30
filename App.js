0e64da35d844a40ffb6dd1c08ac61f8d5649d705
sejal bisen <sejalbisen635@gmail.com>
2026-03-30 09:19:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useRef } from "react";
+import './App.css';
+import anime from 'animejs'
+
+function App() {
+  const containerRef = useRef(null);
+
+  useEffect(() =>{
+    const container = containerRef.current;
+    container.innerHTML = "";
+
+    for(let i=1; i<=100; i++){
+      let dot = document.createElement("div");
+      dot.classList.add("element")
+      container.appendChild(dot);
+    }
+
+    let dotAll = container.querySelectorAll(".element");
+    let animation = anime.timeline({
+      targets: dotAll,
+      easing: "easeInOutExpo",
+      loop: true,
+      delay: anime.stagger(100, { grid: [10, 10], from: "center" }),
+    });
+
+    animation
+      .add({
+        rotateZ: 180, 
+        translateY: anime.stagger(0, { grid: [10, 10], from: "center", axis: "y" }),
+        translateX: anime.stagger(0, { grid: [10, 10], from: "center", axis: "x" }),
+        opacity: 1,
+      })
+
+      .add({
+        borderRadius: 50,
+      })
+
+      .add({
+        scale: 0.2,
+        opacity: 0.2,
+      })
+      
+      .add({
+        rotateZ: 180, 
+        translateY: anime.stagger(0, { grid: [10, 10], from: "center", axis: "y" }),
+        translateX: anime.stagger(0, { grid: [10, 10], from: "center", axis: "x" }),
+        opacity: 1,
+      })
+
+      .add({
+        scale: 1, 
+        borderRadius: 0,
+      })
+
+      .add({
+        rotateZ: -90,
+      })
+
+
+
+  },[])
+  return (
+    <div>
+      {/* header section start */}
+      <header>
+        {/* logo-area */}
+        <a href='#' className='logo'>
+          Logo
+        </a>
+
+        {/* Navigation area */}
+
+        <ul>
+          <li>
+            <a href='#' className='active'>
+              Home
+            </a>
+
+          </li>
+
+          <li>
+            <a href='#'>
+              About
+            </a>
+
+          </li>
+
+          <li>
+            <a href='#'>
+              Work
+            </a>
+          </li>
+          <li>
+            < a href='#'>
+              Conatct
+            </a>
+          </li>
+        </ul>
+      </header>
+      {/* header section end */}
+
+      {/* section hero */}
+      <section>
+        {/* content area start */}
+        <div className='Content'>
+          <h2>
+            Level Up Your Website <b>Anime.js</b>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quibusdam possimus dolorem maiores vitae fuga.
+            Fuga cumque voluptatoo vero commodi quia excepturi eligendi asperioresfugit hic ipsa, dolores explicabo
+            consequuntur neque.
+          </p>
+
+          <a href='#' className='btn'>
+            Learn More 
+          </a>
+        </div>
+         {/* content area end */}
+
+         {/* animated area*/}
+
+         <div className='container' ref={containerRef}></div>
+
+      </section>
+
+
+    </div>
+
+  );
+}
+
+export default App;
